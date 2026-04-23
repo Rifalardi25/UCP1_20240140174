f9ecd2d1c744a86a63b0a7bfa1b4ad6870cf80b1
RifalArdi25 <rifalardi25@gmail.com>
2026-04-23 09:46:06 +0700

    Membuat array dan logika untuk tambah data

---
+++ script.js
@@ -0,0 +1,36 @@
+let dataAnggota = [];
+
+const formDaftar = document.getElementById('formDaftar');
+
+if (formDaftar) {
+    formDaftar.addEventListener('submit', function(event) {
+        event.preventDefault(); // Mencegah halaman reload
+
+    
+        let namaVal = document.getElementById('nama').value;
+        let emailVal = document.getElementById('email').value;
+        let minatVal = document.getElementById('minat').value;
+
+        let anggotaBaru = { nama: namaVal, email: emailVal, minat: minatVal };
+        dataAnggota.push(anggotaBaru);
+
+        // Menampilkan alert
+        alert("Pendaftaran berhasil! Data telah disimpan di array sementara.");
+
+        // Menampilkan hasil input di bagian bawah form
+        const hasilDiv = document.getElementById('hasilInput');
+        hasilDiv.style.display = "block";
+        hasilDiv.innerHTML = `
+            <strong>Data Tersimpan:</strong><br>
+            Nama: ${namaVal} <br>
+            Email: ${emailVal} <br>
+            Minat: ${minatVal} <br>
+            <em>Total anggota di array saat ini: ${dataAnggota.length}</em>
+        `;
+
+        // Reset form setelah disubmit
+        formDaftar.reset();
+        console.log(dataAnggota); // Cek array di console browser
+    });
+}
+
